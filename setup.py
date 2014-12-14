ca90701cca7ea08a34b6f060b3c1271d8add777a
William Gaul <willyg302@gmail.com>
2014-12-14 12:55:36 -1000

    Added some more meta to mkdocs.yml and setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 
-setup(name='clip.py',
-      version='0.2.0',
-      py_modules=['clip'],
-      description="Embeddable, composable command line interface parsing",
-      url="https://github.com/willyg302/clip.py",
-      license='MIT',
-      author="willyg",
-      author_email="willyg302@gmail.com",
-      classifiers=[
-          'Programming Language :: Python :: 2.7',
-      ]
-      )
+setup(
+	name='clip.py',
+	author='William Gaul',
+	author_email='willyg302@gmail.com',
+	version='0.2.0',
+	url='https://github.com/willyg302/clip.py',
+	license='MIT',
+	py_modules=['clip'],
+	description='Embeddable, composable command line interface parsing',
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7',
+	],
+)
